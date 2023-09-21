ef6093aa669ca3a547f7bb25f5fe459a128acd2f
zerxe <amc92v@gmail.com>
2023-09-21 22:58:44 +0100

    asdsa

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hola_mundo/main.dart';
+import 'package:flutter_hola_mundo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
